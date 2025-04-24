e44a81ff820aa681308176435f42a41455b5dc07
rabbiyasana <rabbiasana55@gmail.com>
2025-04-24 17:07:37 +0500

    display posts on detail page

--- blog/all_blogs/urls.py
+++ blog/all_blogs/urls.py
@@ -5,6 +5,6 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.all_blogs, name='all_blogs'),
-
+    path('<int:blog_id>/', views.blog_details, name='blog_details'),
    
 ]
--- blog/all_blogs/views.py
+++ blog/all_blogs/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 def all_blogs(request):
     blogs= Blog.objects.all()
     return render(request, 'all_blogs/all_blogs.html', {'blogs': blogs})
+
+def blog_details(request,blog_id):
+    blog= get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'all_blogs/blog_details.html', {'blog': blog})
